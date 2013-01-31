728f47004bd5c4156a7ec29e09b714bb1ea711b4
Nghia Nguyen <NghiaNguyenBH@gmail.com>
2013-01-31 10:31:19 -0800

    add setup_dot.sh

---
+++ setup_dot.sh
@@ -0,0 +1,12 @@
+# bash config
+cp .bashrc ~/
+cp .bash_aliases ~/
+source ~/.bashrc
+# vim config
+cp .vimrc ~/
+cp -r .vim ~/
+# python start up
+cp .pythonstartup ~/
+# git
+cp .gitconfig ~/
+cp -r .git ~/
