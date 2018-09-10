ccec7a2c6d5d82cd395d852db1507f31f30ffa5d
nednguyen <nednguyen@google.com>
2018-09-10 13:35:49 -0400

    update replace.py script

--- replace.py
+++ replace.py
@@ -9,14 +9,18 @@ def Replace(file_path, old_string, new_string):
     f.write(content.replace(old_string, new_string))
 
 
-def FindAndReplace(root_dir, old_string, new_string):
+def FindAndReplace(root_dir, old_string, new_string, suffix=None):
   root_dir = os.path.abspath(root_dir)
   for root, _, files in os.walk(root_dir):
     for f in files:
       file_path = os.path.abspath(os.path.join(root, f))
       if file_path == os.path.abspath(__file__):
         continue
+      if suffix and not file_path.endswith(suffix):
+        continue
+      print file_path
       try:
         Replace(file_path, old_string, new_string)
       except Exception as e:
         logging.warning('Skip %s because of %s' % (file_path, repr(e)))
+  print 'Done converting %s -> %s' % (repr(old_string), repr(new_string))
